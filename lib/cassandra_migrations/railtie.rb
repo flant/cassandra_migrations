2c2c3852ca722baa1021768fb3f88f54e5a83e06
Timofey.Kirillov <timofey.kirillov@flant.ru>
2014-10-21 20:37:42 +0400

    cassandra_migrations.initializer removed

--- lib/cassandra_migrations/railtie.rb
+++ lib/cassandra_migrations/railtie.rb
@@ -2,10 +2,6 @@
 
 class CassandraMigrations::Railtie < ::Rails::Railtie
   
-  initializer "cassandra_migrations.initializer" do
-    require File.expand_path('railtie/initializer', File.dirname(__FILE__))
-  end
-
   rake_tasks do
     Dir[File.expand_path("railtie/**/*.rake", File.dirname(__FILE__))].each do |file| 
       # 'load' is used because 'require' is only for .rb files
